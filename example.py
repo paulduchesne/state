b2875b6c0cff0231fe3b61d736a4dc397c3cb2b8
paulduchesne <paulduchesne@tuta.io>
2022-12-09 13:50:28 +0100

    added decrypt function

--- example.py
+++ example.py
@@ -5,11 +5,9 @@ state.me('paul duchesne', '1986-04-14')
 
 # identify another individual.
 state.person('philip kindred dick', '1928-12-16')
-'''
+
 # identify file which is owned by individual.
-state.file(file_path)
+# state.file(file_path)
 
 # decrypt all statements in personal graph.
 state.decrypt_all()
-
-'''
--- state/__init__.py
+++ state/__init__.py
@@ -13,12 +13,12 @@ def encryption_pair():
     statement_uuid = str(uuid.uuid4()).replace('-','')
     key_uuid = str(uuid.uuid4()).replace('-','')
 
-    config_location = pathlib.Path.home() / 'individual.json'
+    config_location = pathlib.Path.cwd() / 'individual.json'
     with open(config_location) as config:
         config = json.load(config)
 
     pull_keys = config['keys']
-    pull_keys.append({statement_uuid: key_uuid})
+    pull_keys[statement_uuid] = key_uuid
     config['keys'] = pull_keys
 
     with open(config_location, 'w') as config_out:
@@ -30,11 +30,11 @@ def me(name, birth):
 
     ''' Define individual. '''
     
-    config_location = pathlib.Path.home() / 'individual.json'
+    config_location = pathlib.Path.cwd() / 'individual.json'
     if config_location.exists():
         print('Individual already defined.')
     else:
-        individual = {"uuid":str(uuid.uuid4()), "name":name, "birth":birth, "keys":[]}
+        individual = {"uuid":str(uuid.uuid4()), "name":name, "birth":birth, "keys":{}}
         with open(config_location, 'w') as config:
             json.dump(individual, config)
 
@@ -42,7 +42,7 @@ def individual():
 
     ''' Retrieve state config file. '''
 
-    config_location = pathlib.Path.home() / 'individual.json'
+    config_location = pathlib.Path.cwd() / 'individual.json'
     if not config_location.exists():
         raise Exception('Individual undefined. Create with state.me(name, dob)')
     else:
@@ -54,7 +54,6 @@ def statement(triple, home):
 
     ''' Encrypt rdf triple as meta-triple. '''
 
-    enc = encryption_pair()
     ont = rdflib.Namespace(f'https://{home}.org/ontology/') 
     res = rdflib.Namespace(f'https://{home}.org/resource/')
 
@@ -62,15 +61,17 @@ def statement(triple, home):
     single.add(triple)
     source = str(single.serialize(format='ntriples'))
 
+    enc = encryption_pair()
     fernet = Fernet(base64.urlsafe_b64encode(enc['key'].encode()))
 
+    ttl_file = pathlib.Path.cwd() / 'graph.ttl'
     meta_graph = rdflib.Graph()
+    if ttl_file.exists():
+        meta_graph.parse(ttl_file)
+
     meta_graph.add((res[enc['statement']], rdflib.RDF.type, ont['statement'] ))
     meta_graph.add((res[enc['statement']], ont['has_payload'], rdflib.Literal(fernet.encrypt(source.encode()).decode())))
-
-    print(meta_graph.serialize(format='turtle'))
-
-    # this needs to be appended to local turtle file.
+    meta_graph.serialize(destination=str(ttl_file), format="turtle")
 
 def person(name, birth):
 
@@ -86,5 +87,36 @@ def person(name, birth):
     statement((res[person_uuid], ont['has_name'], rdflib.Literal(name)), home_uuid)
     statement((res[person_uuid], ont['has_birth_date'], rdflib.Literal(birth)), home_uuid)
 
-# decMessage = fernet.decrypt(encMessage.encode()).decode()
-# print(decMessage)
+def decrypt_all():
+
+    ''' Decrypt metagraph. '''
+
+    home_uuid = individual()
+    ont = rdflib.Namespace(f'https://{home_uuid}.org/ontology/') 
+    res = rdflib.Namespace(f'https://{home_uuid}.org/resource/')
+
+    meta_graph = rdflib.Graph()
+    meta_graph.parse(pathlib.Path.cwd() / 'graph.ttl')
+
+    statements = [s for s,p,o in meta_graph.triples((None, None, ont.statement))]
+
+    payloads = list()
+    for x in statements:
+        statement_id = pathlib.Path(str(x)).stem
+        for s,p,o in meta_graph.triples((x, ont['has_payload'], None)):
+            payloads.append({statement_id:str(o)})
+
+    config_location = pathlib.Path.cwd() / 'individual.json'
+    with open(config_location) as config:
+        keys = json.load(config)['keys']
+
+    lower_graph = rdflib.Graph()
+    for p in payloads:
+        for k,v in p.items():
+            fernet = Fernet(base64.urlsafe_b64encode(keys[k].encode()))
+            decMessage = fernet.decrypt(v.encode()).decode()
+            decode_graph = rdflib.Graph()
+            decode_graph.parse(data=decMessage)
+            lower_graph += decode_graph
+
+    print(lower_graph.serialize(format='turtle'))
