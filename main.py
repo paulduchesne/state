6fbdac6681dd9dab0a409536ca3dfe5497a6473c
paulduchesne <paulduchesne@tuta.io>
2025-11-10 13:12:29 +1100

    data fixes

--- main.py
+++ main.py
@@ -17,15 +17,21 @@ def build():
 
     entities = list()
     for e in [
-        "http://www.loc.gov/premis/rdf/v3/Event",
+        "http://www.loc.gov/premis/rdf/v3/Event", # this needs to be generalised to include all subclasses.
         "http://www.loc.gov/premis/rdf/v3/Person",
         "http://www.loc.gov/premis/rdf/v3/Organization",
+        "https://paulduchesne.github.io/personal-premis/ontology/Book",
     ]:
         entities += [s for s,p,o in g.triples((None, rdflib.RDF.type, rdflib.URIRef(e)))]
+
+    for e in entities:
+        print(e)
 
     # build markdown.
 
     for e in entities:
+
+        # TODO introduce expectation of minimum data here.
 
         string = ''
 
@@ -37,7 +43,8 @@ def build():
             comment = o
         string += comment
 
-        markdown_path = pathlib.Path.cwd() / f'{'/'.join(pathlib.Path(e).parts[2:])}.md'
+        markdown_path = pathlib.Path.cwd() / f'{'/'.join(pathlib.Path(e).parts[3:])}.md'
+        markdown_path.parent.mkdir(exist_ok=True)
         with open(markdown_path, 'w') as markdown:
             markdown.write(string)
 
