86d04598d71e1a01b431f163cd44cc95ab582f19
paulduchesne <paulduchesne@tuta.io>
2026-03-19 06:13:48 +1100

    index page label margin

--- static/nodes.js
+++ static/nodes.js
@@ -5,7 +5,7 @@ async function drawNodes(nodes) {
     .data(nodes)
     .enter()
     .append("svg")
-    .attr('width', 600)
+    .attr('width', 1200)
     .attr('height', d => {
       const rowCount = Math.ceil(d.instances.length / 24);
       const lowestY = (rowCount - 1) * 25 + 40;
@@ -14,8 +14,8 @@ async function drawNodes(nodes) {
 
   section.append('text')
     .text(d => d.class_label)
-    .attr('x', 0).attr('y', 30)
-    .attr('class', 'regular_text')
+    .attr('x', 300).attr('y', 32)
+    .attr('class', 'type_labels')
 
   section.selectAll("circle")
     .data(d => d.instances)
@@ -24,7 +24,7 @@ async function drawNodes(nodes) {
     .text(d => d.entity_label)
     .attr('class', 'regular_text circle_labels')
     .attr('id', (d, i) => 'text_' + d.entity_link.split('/').pop())
-    .attr("x",(d,i) => ((i % 24)*25)+11+15)
+    .attr("x",(d,i) => ((i % 24)*25)+11+15+300)
     .attr("y", (d, i) => (Math.floor(i / 24) * 25) + 60+5)
     .attr("opacity",0)
 
@@ -35,7 +35,7 @@ async function drawNodes(nodes) {
     .attr('class', 'round')
     .attr('id', (d,i) => d.entity_link.split('/').pop())
     .attr("r", 10)
-    .attr("cx",(d,i) => ((i % 24)*25)+11)
+    .attr("cx",(d,i) => ((i % 24)*25)+11+300)
     .attr("cy", (d,i) => (Math.floor(i / 24)*25)+60)
     .attr('fill', "#dddddd")
     .attr('stroke', "black")
@@ -43,11 +43,13 @@ async function drawNodes(nodes) {
     .attr('opacity', 0.8)
     .on('mouseover', function (k, d) {
       d3.selectAll('.round').transition().duration(200).attr('opacity', 0.1);
+      d3.selectAll('.type_labels').transition().duration(200).attr('opacity', 0.1);
       d3.select(this).transition().duration(200).attr('opacity', 0.8);;
       d3.select('#'+'text_'+d.entity_link.split('/').pop()).transition().duration(200).attr('opacity', 1)
     })
     .on('mouseout', function (k, d) {
       d3.selectAll('.round').transition().duration(200).attr('opacity', 0.8);
+      d3.selectAll('.type_labels').transition().duration(200).attr('opacity', 0.8);
       d3.selectAll('.circle_labels').transition().duration(200).attr('opacity', 0);
     })
     .on('click', function (k, d) {
